214ff8ae48b0490506c46ea0955d125b17e45abd
smythe.spaer <smythe.spaer@gmail.com>
2021-09-10 17:29:48 +0300

    Store is done

--- src/AppRedux.js
+++ src/AppRedux.js
@@ -1,52 +1,32 @@
 import './App.css';
-import React , { useEffect, useState } from 'react';
-import { createStore } from 'redux';
+import React  from 'react';
 import ToDoForm from './TodoForm';
 import TodoList from './TodoList';
+import {AddTodoAction,deleteTodoAction,changeStatusAction} from './actionCreators';
 
 
-
-function AddTodoAction(text) {
-    const trimmedText = text.trim();
-
-    const taskItem = {
-        task: trimmedText,
-        isDone: false,
-    };
-
-    if(trimmedText.length > 0)
-    {
-        return { type: 'todos/add', value: taskItem};
-    } else {
-        return ;
-    }
-}
-
-function todoReducer(state = [], action){
-    switch(action.type){
-        case 'todos/add':{            
-            return [...state,action.value];
-        }
-        default:
-            return state;
-    }
-}
-
-const initialState = [];
-const store = createStore(todoReducer,initialState);
-
-const RApp = () => {
+const RApp = ({store}) => {
 
 
     return (<div className='App'>
-        <h1>Todo</h1>
-        <ToDoForm 
-            saveTodo = { taskText => {
-                store.dispatch(AddTodoAction(taskText));
-                console.log(store.getState());
-            }}
-        />
-        <TodoList todos = {store.getState()}></TodoList>
+            <div className = 'List'>
+                <h1>Todo</h1>
+                <ToDoForm 
+                    saveTodo = { taskText => {
+                        store.dispatch(AddTodoAction(taskText));
+                        console.log(store.getState());
+                    }}
+                />
+                <TodoList 
+                    todos = {store.getState()}
+                    changeStatus = {index => {
+                        store.dispatch(changeStatusAction(index));
+                    }}
+                    deleteTodo = {index => {
+                        store.dispatch(deleteTodoAction(index));
+                    }}
+                ></TodoList>
+            </div>
     </div>);
 }
 
--- src/index.js
+++ src/index.js
@@ -1,18 +1,50 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { createStore } from 'redux';
 import App from './App';
 import RApp from './AppRedux';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <RApp />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
+function todoReducer(state = [], action){
+  switch(action.type){
+      case 'todos/add':{            
+          return [...state,action.value];
+      }
+      case 'todos/changeStatus':{
+        return state.map( (val,i) => {
+          if ( i === action.value){
+            val.isDone = !val.isDone;
+            return val;
+          } else {
+            return val;
+          }
+        })
+      }
+      case 'todos/delete':{
+        return state.filter( (val,i) => i !== action.value)
+      }
+      default:
+          return state;
+  }
+}
+
+const initialState = [];
+const store = createStore(todoReducer,initialState);
+
+function render(){
+    ReactDOM.render(
+      <React.StrictMode>
+        <RApp store={store} />
+      </React.StrictMode>,
+      document.getElementById('root')
+    );
+}
+
+store.subscribe(render);
+
+render();
+
 reportWebVitals();
